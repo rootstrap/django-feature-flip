ed8a9e3b7418451891f142798dbedb092344ca14
Martin Jaime <martin.jaime@rootstrap.com>
2020-07-30 14:34:09 -0300

    Add admin for features

---
+++ django_feature_flip/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django_feature_flip.models import Feature
+
+
+class FeatureAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Feature, FeatureAdmin)
