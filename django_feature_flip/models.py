9e68c88c2ab0cbb1cd38d6c28ac375ecbb8c8fe0
Martin Jaime <martin.jaime@rootstrap.com>
2020-07-31 17:23:52 -0300

    Feature: enable/disable feature by % of time

--- django_feature_flip/models.py
+++ django_feature_flip/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Group(models.Model):
@@ -11,6 +12,9 @@ class Group(models.Model):
 class Feature(models.Model):
     name = models.CharField(max_length=40, unique=True, db_index=True, null=False)
     totally_enabled = models.BooleanField(default=False, null=False)
+    time_percentage = models.IntegerField(
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
     groups = models.ManyToManyField(Group, related_name='features')
 
     def __str__(self):
