0960a35cd2014faec6ec4987ab9752ec6108f0f4
Martin Jaime <martin.jaime@rootstrap.com>
2020-07-31 09:58:32 -0300

    Feature: add groups

--- django_feature_flip/helpers.py
+++ django_feature_flip/helpers.py
@@ -1,11 +1,15 @@
 
-from django_feature_flip.models import Feature
+from django_feature_flip.models import Group, Feature
 
 
 class FeatureFlip:
+    class FeatureFlipError(Exception):
+        def __init__(self, message):
+            self.message = message
+
     class __FeatureFlip:
         def __init__(self):
-            pass
+            self.groups = {}
 
         def __str__(self):
             return repr(self)
@@ -19,4 +23,39 @@ class FeatureFlip:
         return getattr(self.instance, name)
 
     def enabled(self, feature_name, actor=None):
-        return Feature.objects.get(name=feature_name).enabled(actor)
+        feature = get_feature_or_raise_error(feature_name)
+
+        if feature.totally_enabled:
+            return True
+        elif actor:
+            return self.feature_enabled_for_a_group_of_actor(feature, actor)
+        else:
+            return False
+
+    def register(self, group_name, func):
+        Group.objects.get_or_create(name=group_name)
+
+        self.groups[group_name] = func
+
+    def activate_group(self, feature_name, group_name):
+        feature = get_feature_or_raise_error(feature_name)
+
+        get_group_or_raise_error(group_name).features.add(feature)
+
+    def feature_enabled_for_a_group_of_actor(self, feature, actor):
+        groups_names = [group.name for group in feature.group_set.all()]
+        return any(self.groups[group_name](actor) for group_name in groups_names)
+
+
+def get_feature_or_raise_error(feature_name):
+    try:
+        return Feature.objects.get(name=feature_name)
+    except Feature.DoesNotExist:
+        raise FeatureFlip.FeatureFlipError(f'Feature {feature_name} does not exist')
+
+
+def get_group_or_raise_error(group_name):
+    try:
+        return Group.objects.get(name=group_name)
+    except Group.DoesNotExist:
+        raise FeatureFlip.FeatureFlipError(f'Group {group_name} does not exist')
--- django_feature_flip/tests.py
+++ django_feature_flip/tests.py
@@ -34,13 +34,55 @@ class FeatureFlipTestCase(TestCase):
         self.assertEqual(id(self.feature_flip), id(self.feature_flip))
 
     def test_enabled_for_not_totally_enabled_feature(self):
-        FeatureFactory(name='my_feature', totally_enabled=False)
+        FeatureFactory(name='my_enabled_feature')
         actor = self._actor(1)
 
-        self.assertFalse(self.feature_flip.enabled('my_feature', actor))
+        self.assertFalse(self.feature_flip.enabled('my_enabled_feature', actor))
 
     def test_enabled_for_totally_enabled_feature(self):
-        FeatureFactory(name='my_feature', totally_enabled=True)
+        FeatureFactory(name='my_disabled_feature', totally_enabled=True)
         actor = self._actor(1)
 
-        self.assertTrue(self.feature_flip.enabled('my_feature', actor))
+        self.assertTrue(self.feature_flip.enabled('my_disabled_feature', actor))
+
+    def test_enabled_for_non_existent_feature(self):
+        actor = self._actor(1)
+        try:
+            self.feature_flip.enabled('non_existent_feature', actor)
+        except FeatureFlip.FeatureFlipError as error:
+            self.assertEqual(error.message, 'Feature non_existent_feature does not exist')
+
+    def test_group_registration(self):
+        FeatureFactory(name='my_feature', totally_enabled=False)
+        FeatureFactory(name='my_other_feature', totally_enabled=True)
+
+        # we register a group of all actors with flip_id() > 5
+        self.feature_flip.register('flip_id_gt_5', lambda obj: obj.flip_id() > 5)
+
+        # we register a group of all actors with flip_id() <= 5
+        self.feature_flip.register('flip_id_lte_5', lambda obj: obj.flip_id() <= 5)
+
+        disabled_actor = self._actor(1)
+        enabled_actor = self._actor(6)
+
+        self.feature_flip.activate_group('my_feature', 'flip_id_gt_5')
+        self.feature_flip.activate_group('my_other_feature', 'flip_id_lte_5')
+
+        self.assertTrue(FeatureFlip().enabled('my_feature', enabled_actor))
+        self.assertFalse(FeatureFlip().enabled('my_feature', disabled_actor))
+
+    def test_activate_group_for_non_existent_feature(self):
+        self.feature_flip.register('my_group', lambda obj: obj.flip_id() > 5)
+
+        try:
+            self.feature_flip.activate_group('non_existent_feature', 'my_group')
+        except FeatureFlip.FeatureFlipError as error:
+            self.assertEqual(error.message, 'Feature non_existent_feature does not exist')
+
+    def test_activate_group_for_non_existent_group(self):
+        FeatureFactory(name='my_feature')
+
+        try:
+            self.feature_flip.activate_group('my_feature', 'non_existent_group')
+        except FeatureFlip.FeatureFlipError as error:
+            self.assertEqual(error.message, 'Group non_existent_group does not exist')
