bd9e0d5b9fcec010b18af682b38ba35414dd2295
Martin Jaime <martin.jaime@rootstrap.com>
2020-07-31 09:58:32 -0300

    Feature: add groups

--- django_feature_flip/models.py
+++ django_feature_flip/models.py
@@ -5,8 +5,13 @@ class Feature(models.Model):
     name = models.CharField(max_length=40, unique=True)
     totally_enabled = models.BooleanField(default=False)
 
-    def enabled(self, actor):
-        return self.totally_enabled
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=40, unique=True, null=False)
+    features = models.ManyToManyField(Feature)
 
     def __str__(self):
         return self.name
